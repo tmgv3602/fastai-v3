32818c76605cec1b4c0110ddc0206fa25ccec4c0
Takahiro Yamanaka <tacahiro@jcom.home.ne.jp>
2020-01-04 00:01:46 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1X1Pb9jlBTWfCjxk8SzcfeM6Wy0-S4FOn?alt=media&key=AIzaSyBXHfp6fKRJmTDnCaIZpvHIi8zZ-1pUCoM'
-export_file_name = 'my_classifier.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1x7ZzoxUcaaTePAzwDq7kfZkgSE89LCii?alt=media&key=AIzaSyBXHfp6fKRJmTDnCaIZpvHIi8zZ-1pUCoM'
+export_file_name = 'my_classifier.h5'
 
 classes = ['benign', 'malignant']
 path = Path(__file__).parent

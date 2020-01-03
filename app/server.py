922e2c9caff66d17990797d2bb4d96308b42f6e6
Takahiro Yamanaka <tacahiro@jcom.home.ne.jp>
2020-01-03 15:48:54 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1X1Pb9jlBTWfCjxk8SzcfeM6Wy0-S4FOn?alt=media&key=AIzaSyBXHfp6fKRJmTDnCaIZpvHIi8zZ-1pUCoM'
+export_file_name = 'my_classifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['benign', 'malignant']
 path = Path(__file__).parent
 
 app = Starlette()
